d1b99212eaca5a1457dc96ec5aa8df68dbadf042
Ryan Applegate <ryan.applegate@gmail.com>
2013-07-31 17:49:16 -0700

    Removed menu from polyshape main(). Added docstrings to polyutil.

--- polyshape.cpp
+++ polyshape.cpp
@@ -8,56 +8,24 @@ using namespace std;
 
 #include "Stack.cpp"
 #include "polyutil.cpp"
-void showMenu() {
-		cout << "\tUsage: polyshape.e #cells #shapetype\n";
-		cout << "\t#cells range: 1 to 13.\n";
-		cout << "\t#shapetypes:\n";
-		cout << "\t (1) polyomino: squares that share a side.\n";
-		cout << "\t (2) polyplet: squares that share a corner or a side.\n";
-		cout << "\t (3) polyhex: hexagons that share a side.\n";
-		cout << "\t (4) polyiamond: triangles that share a side.\n"; 
-}
+
 
 int main(int argc, char* argv[]) {
 
 	cout << "  This program generates polyshapes.\n";
-	if(argc !=3) {
-		showMenu();
-		return 1;
-	}
-	else {
-		int N_site = atoi(argv[1]);
-		if( N_site < 1 || N_site > 13) {
-			cout << "\tYour #cells was not a valid number.\n";
-			showMenu();
-			return 1;
-		}
-		if( atoi(argv[2]) == 1) {
-			cout << "We will now generate polyominoes with " << N_site << " cells.\n";
-			int count = generatePolyShape(N_site,polyominoNeighbors);
-			cout << "The number of polyominoes with " << N_site << " cells is " << count << endl;
-		}
-		else if( atoi(argv[2]) == 2){
-			cout << "We will now generate polyplets with " << N_site << " cells.\n";
-			int count = generatePolyShape(N_site,polypletNeighbors);
-			cout << "The number of polyplets with " << N_site << " cells is " << count << endl;
-		}
-		else if( atoi(argv[2]) == 3){
-			cout << "We will now generate polyhexes with " << N_site << " cells.\n";
-			int count = generatePolyShape(N_site,polyhexNeighbors);
-			cout << "The number of polyhexes with " << N_site << " cells is " << count << endl;
-		}
-		else if( atoi(argv[2]) == 4){
-			cout << "We can't generate polyiamonds yet\n";
-			//cout << "We will now generate polyiamonds with " << N_site << " cells.\n";
-			//int count = generatePolyShape(N_site,polyiamondNeighbors);
-			//cout << "The number of polyiamond with " << N_site << " cells is " << count << endl;
-		}
-		else {
-			cout << "No valid shape specified.\n";
-			showMenu();
-		}
-	}//else
+    int N_site = atoi(argv[1]);
+    cout << N_site << endl;
 
+
+//polyominoes
+    cout << "\nominoes: " << generatePolyShape(N_site,polyominoNeighbors);
+//polyplets
+    cout << "\nplets: " << generatePolyShape(N_site,polypletNeighbors);
+//polyhexes
+    cout << "\nhex: " << generatePolyShape(N_site,polyhexNeighbors);
+//polyiamonds
+    //int count = generatePolyShape(N_site,polyiamondNeighbors);
+
+    cout << endl;
 return 0;
 }//main
--- polyutil.cpp
+++ polyutil.cpp
@@ -3,7 +3,8 @@ struct Node{
 	int j;
 };
 
-void print_avail(bool **avail,int N_site){
+void print_avail(bool **avail, int N_site)
+{
 	for(int i = 0; i < N_site; i++) {
 		for(int j = 0; j < 2*N_site-1; j++)
 			cout << avail[i][j] << " ";
@@ -11,8 +12,20 @@ void print_avail(bool **avail,int N_site){
 	}
 }
 
-bool IsValid(Node neighbor, int const N_site) {
-	//it's in the bounds of the grid
+bool IsValid( Node neighbor, int const N_site )
+{
+    /*'
+    Parameters
+    ----------
+    neighbor:   Node, proposed as valid part of grid with members (i,j)
+    N_site:     int, marking grid boundary
+
+    Returns
+    -------
+    TRUE:       if valid cell in grid
+    FALSE:      if NOT valid cell in grid
+
+    '*/
 	if( neighbor.j >=2*N_site-1 )
 		return 0;
 	if( neighbor.j < 0 )
@@ -21,72 +34,125 @@ bool IsValid(Node neighbor, int const N_site) {
 		return 0;
 	if( neighbor.i < 0 )
 		return 0;
+
 	return 1;
-}//bool IsValid
+}// bool IsValid
 
-int polypletNeighbors(Node v,Stack<Node> &stack,bool **avail,int const N_site) {
+int polypletNeighbors( Node v, Stack<Node> &stack,
+                       bool **avail, int const N_site )
+{
+    /*'
+    Parameters
+    ----------
+    v:          the Node around which we find neighbor cells
+    stack:      a LIFO structure holding all prev added neighbors
+    avail:      tracks if a cell is being used already
+    N_site:     defines size of some variables
+    
+    Returns
+    -------
+    num_added:  the count of the number of neighbors added in this call
+
+    DETAILS
+        Adds cells of a square lattice using the order below
+        starting with cell 0 to the current stack. Cells which are not
+        available are skipped.
+        . . . . .
+        . 5 6 7 .
+        . 4 v 0 .
+        . 3 2 1 .
+        . . . . .
+    '*/
+
 	Node n;
 	int num_added = 0;
-	n.i = v.i;
+
+	n.i = v.i;                                  // right
 	n.j = v.j+1;
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i+1;
-	n.j = v.j+1;
+	n.i = v.i+1;                                // lower
+	n.j = v.j+1;                                // right
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i+1;
-	n.j = v.j;
+	n.i = v.i+1;                                // lower
+	n.j = v.j;                                  //
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i+1;
-	n.j = v.j-1;
+	n.i = v.i+1;                                // lower
+	n.j = v.j-1;                                // left
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i;
-	n.j = v.j-1;
+	n.i = v.i;                                  //
+	n.j = v.j-1;                                // left
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i-1;
-	n.j = v.j-1;
+	n.i = v.i-1;                                // upper
+	n.j = v.j-1;                                // left
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i-1;
-	n.j = v.j;
+	n.i = v.i-1;                                // upper
+	n.j = v.j;                                  //
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
-	n.i = v.i-1;
-	n.j = v.j+1;
+	n.i = v.i-1;                                // upper
+	n.j = v.j+1;                                // right
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
 		stack.Push(n);
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
+
 	return num_added;
 }
 
-int polyominoNeighbors(Node v,Stack<Node> &stack,bool **avail,int const N_site) {
+int polyominoNeighbors( Node v, Stack<Node> &stack,
+                        bool **avail, int const N_site )
+{
+    /*'
+    Parameters
+    ----------
+    v:      the Node around which we find neighbor cells
+    stack:  a LIFO structure holding all prev added neighbors
+    avail:  tracks if a cell is being used already
+    N_site: defines size of some variables
+    
+    Returns
+    -------
+    num_added:  the count of the number of neighbors added in this call
+
+    DETAILS
+        Adds cells of a square lattice using the order below
+        starting with cell 0 to the current stack. Cells which are not
+        available are skipped.
+        . . . . .
+        . . 3 . .
+        . 2 v 0 .
+        . . 1 . .
+        . . . . .
+    '*/
+
 	Node n;
 	int num_added = 0;
 	n.i = v.i;
@@ -117,12 +183,46 @@ int polyominoNeighbors(Node v,Stack<Node> &stack,bool **avail,int const N_site) 
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
+
 	return num_added;
 }
 
-int polyhexNeighbors(Node v,Stack<Node> &stack,bool **avail,int const N_site) {
+int polyhexNeighbors( Node v, Stack<Node> &stack,
+                      bool **avail, int const N_site )
+{
+    /*
+    Parameters
+    ----------
+    v:      the Node around which we find neighbor cells
+    stack:  a LIFO structure holding all previously added neighbors
+    avail:  FALSE if a cell is being used already
+    N_site: defines size of grid related variables
+    
+    Returns
+    -------
+    num_added:  the count of the number of neighbors added in this call
+
+    DETAILS
+        Adds cells of a honeycomb lattice using the order below
+        starting with cell 0 to the current stack. Cells which are not
+        available are skipped.
+
+        The lattice is mapped to a square lattice as follows...
+
+         . . . . .              . . . . .
+        . . 4 5 r .             . 4 5 r .
+         . 3 v 0 .      -->     . 3 v 0 .
+        . q 2 1 . .             . q 2 1 .
+         . . . . .              . . . . .
+
+        cell v: (i,j) does not neigbhor:
+                 r: (i-1,j+1) 
+                 q: (i+1,j-1)
+        
+    */
 	Node n;
 	int num_added = 0;
+
 	n.i = v.i;
 	n.j = v.j+1;
 	if( IsValid(n,N_site) && avail[n.i][n.j] ) {
@@ -165,12 +265,25 @@ int polyhexNeighbors(Node v,Stack<Node> &stack,bool **avail,int const N_site) {
 		avail[n.i][n.j] = 0;
 		num_added++;
 	}
+
 	return num_added;
 }
 
 void explore(Stack<Node> &stack,bool **avail,Node *path,int depth,
- int const N_site,	int &graph_count,
- int polyshapeNeighbors(Node,Stack<Node> &,bool**,int const)) {
+  int const N_site,	int &graph_count,
+  int polyshapeNeighbors(Node,Stack<Node> &,bool**,int const) )
+{
+    /*
+    Parameters
+    ----------
+    stack:          LIFO list, holds explored cells
+    avail:          FALSE, for used cells
+    path:           Node array pointer, holds nodes in current graph
+    depth:          int, monitors depth of recursion
+    N_site:         int, defines size of grid related variables
+    graph_count:    
+
+    */
 	//precondition stack is NOT empty
 	Node v = stack.Pop();
 	path[depth] = v;
@@ -202,9 +315,33 @@ void explore(Stack<Node> &stack,bool **avail,Node *path,int depth,
 }//void explore, recursive
 
 int generatePolyShape(int const N_site,
- int polyshapeNeighbors(Node,Stack<Node> &,bool**,int const) ){
+  int polyshapeNeighbors(Node,Stack<Node> &,bool**,int const) )
+{
+	/*'
+    Parameters
+    ----------
+    N_site:                 int, defines grid size
+    polyshapeNeighbors():   function, defines neighbor rules
 
+    Returns
+    -------
+    graph_count:            int, total graphs with N_site cells
+
+    DETAILS
+             <---- 2*N_site-1 --->
+        ^    X X ... X S 0 0 ... 0
+        |    0 0 ... 0 0 0 0 ... 0 
+    N_site   0 0 ... . . . . ... 0 
+        |    . . ... . . . . ... . 
+        v    0 0 ... . . . . ... 0
+
+			X: not allowed
+			0: available
+			S: starting cell for all graphs 
+
+	'*/
 	bool **avail = new bool*[N_site];
+
 	for(int i = 0; i < N_site; i++) {
 		avail[i] = new bool[2*N_site-1];
 		for(int j = 0; j < 2*N_site-1; j++) {
@@ -212,36 +349,21 @@ int generatePolyShape(int const N_site,
 		}
 	}
 	for(int j = 0; j < N_site-1; j++) {
-		avail[0][j] = 0;//depicted as an X below
+		avail[0][j] = 0;
 	}
-	/*
-			<---- 2*N_site-1 --->
-		^	   X X ... X S 0 0 ... 0
-		|	   0 0 ... 0 0 0 0 ... 0 
-	N_site 0 0 ... . . . . ... 0 
-		|    . . ... . . . . ... . 
-		v	   0 0 ... . . . . ... 0
 
-			X: not allowed
-			0: available
-			S: starting cell for all graphs 
-
-	*/
 	Node *path = new Node[N_site];
 	Stack<Node> stack;
 
 	Node start;
-	start.i = 0;
+	start.i = 0;                        	//start S, @ (0,N_site-1)
 	start.j = N_site-1;
-	//start node, S, has coordinates (0,N_site-1)
-	stack.Push(start);
-	//initial condition place node S on the empty stack
-	avail[0][N_site-1] = 0;
-	//mark S as unavailable
+
+	stack.Push(start);                      //initilize S to stack
+	avail[0][N_site-1] = 0;                 //mark S as unavailable
 
 	int graph_count = 0;
 	int depth = 0;
-	//there are zero graphs to start and the length of the exploration is zero
 
 	explore(stack,avail,path,depth,N_site,graph_count,polyshapeNeighbors);
 
